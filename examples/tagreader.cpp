b9dae17b62e3fe5e94e470601a1f402aee40b053
David Eisner <david.eisner@oriel.oxon.org>
2011-11-04 12:08:28 +0000

    Picture reading example

--- examples/tagreader.cpp
+++ examples/tagreader.cpp
@@ -27,6 +27,9 @@
 
 #include <fileref.h>
 #include <tag.h>
+#include <flac/flacfile.h>
+#include <ogg/oggfile.h>
+#include <ogg/xiphcomment.h>
 
 using namespace std;
 
@@ -57,6 +60,32 @@ int main(int argc, char *argv[])
       cout << "comment - \"" << tag->comment() << "\"" << endl;
       cout << "track   - \"" << tag->track()   << "\"" << endl;
       cout << "genre   - \"" << tag->genre()   << "\"" << endl;
+      TagLib::Ogg::XiphComment *comment = NULL;
+      TagLib::FLAC::File *flac = dynamic_cast<TagLib::FLAC::File *>(f.file());
+      if (flac) {
+        cout << "flac:" << endl;
+        cout << "id3v1   - \"" << flac->ID3v1Tag()   << "\"" << endl;
+        cout << "id3v2   - \"" << flac->ID3v2Tag()   << "\"" << endl;
+        cout << "xiph    - \"" << flac->xiphComment()   << "\"" << endl;
+        comment = flac->xiphComment();
+      }
+      if (!comment) {
+        comment = dynamic_cast<TagLib::Ogg::XiphComment *>(tag);
+      }
+      if (comment) {
+        TagLib::Ogg::FieldListMap fields = comment->fieldListMap();
+        for(TagLib::Ogg::FieldListMap::ConstIterator it = fields.begin(), end = fields.end(); it != end; it++) {
+          if (!it->second.isEmpty())
+            cout << "xiph:" << it->first << " \"" << it->second[0].substr(0,3) << "\"" << endl;
+        }
+      }
+      cout << "pictures- \"" << f.file()->pictures().size()   << "\"" << endl;
+      TagLib::File::PictureList l = f.file()->pictures();
+      for (TagLib::File::_PictureList::ConstIterator i = l.begin(), end = l.end(); i != end; i++) {
+        cout << "\t" << (*i)->typeName() << ' ' << (*i)->mimeType() << endl;
+      }
+      
+      cout << "pictures- \"" << tag->pictures().size()   << "\"" << endl;
     }
 
     if(!f.isNull() && f.audioProperties()) {
